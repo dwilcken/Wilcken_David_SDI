203acaed6aa8a11616dbd6f8a287e126eabc5a85
david wilcken <dwilcken@gmail.com>
2015-08-05 14:05:45 -0400

    Added Alerts
    Added alerts to each variable for the output assignment.

--- Setup GitHub/Wilcken_David_Output_Assignment/js/script.js
+++ Setup GitHub/Wilcken_David_Output_Assignment/js/script.js
@@ -4,15 +4,21 @@
 //Output Assignment
 
 //String variable
+alert ("What city do I currently Live ?in");
 var currentCity = "Key West Florida";
+console.log (currentCity);
 
 //Number variable
+alert ("How many years have I been in the Air Force?");
 var numberOfYearsInTheAirForce = 7;
+console.log (numberOfYearsInTheAirForce);
 
 //Boolean variable
+alert ("Do I like Mexican food?");
 var mexicanFoodIsMyFavorite = true;
+console.log (mexicanFoodIsMyFavorite);
 
 //Just for fun Arrays
-var myFavoriteHobbies = ["Bagpipes","Shooting","Woodworking","JavaScript"];
-
-console.log(myFavoriteHobbies[0]);
+alert ("One of my favorite hobbies");
+var oneOfmyFavoriteHobbies = ["Bagpipes","Shooting","Woodworking","JavaScript"];
+console.log(oneOfmyFavoriteHobbies[0]);
