db9e944654ed99ed9af3b0c6e69cd9a927c275a9
david wilcken <dwilcken@gmail.com>
2015-08-05 13:49:19 -0400

    String Variable
    Output assignment String variable

--- Setup GitHub/Wilcken_David_Output_Assignment/js/script.js
+++ Setup GitHub/Wilcken_David_Output_Assignment/js/script.js
@@ -4,11 +4,10 @@
 //Output Assignment
 
 //String variable
-
+var currentCity = "Key West Florida";
 
 //Number variable
 var numberOfYearsInTheAirForce = 7;
-
 
 //Boolean variable
 var mexicanFoodIsMyFavorite = true;
