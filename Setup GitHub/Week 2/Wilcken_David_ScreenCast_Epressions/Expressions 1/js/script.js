bb6f15a584103ddd15f7516b3e9a225853810cc8
david wilcken <dwilcken@gmail.com>
2015-08-06 11:20:33 -0400

    Arithmetic Operators
    Arithmetic Operators

--- Setup GitHub/Week 2/Wilcken_David_ScreenCast_Epressions/Expressions 1/js/script.js
+++ Setup GitHub/Week 2/Wilcken_David_ScreenCast_Epressions/Expressions 1/js/script.js
@@ -9,3 +9,10 @@ console.log(a);
 var yearBorn = 1983;
 var age = 2015 - yearBorn - 1;
 console.log(age);
+
+//Expressions - Arithmetic Operators
+//area of the trinagle is half the width times the height
+var width = 4;
+var height = 5;
+var area = width * height/2;
+console.log(area);
