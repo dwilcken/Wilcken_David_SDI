a62d2f6c0c2b076021b28e762a918ac465b51182
david wilcken <dwilcken@gmail.com>
2015-08-07 09:06:29 -0400

    Prompt and Alert
    Prompt and Alert exercise

--- Setup GitHub/Week 2/Wilcken_David_ScreenCast_Prompt&Alert/js/script.js
+++ Setup GitHub/Week 2/Wilcken_David_ScreenCast_Prompt&Alert/js/script.js
@@ -1,3 +1,9 @@
-/**
- * Created by davidwilcken on 8/7/15.
- */
+//Prompt and Alert
+
+
+var width = prompt("We are calculating the area of a rectangle. \n Please enter the width");
+var height = prompt("Please eneter the height");
+var area = width * height;
+//console.log(area);
+
+alert("The area of your rectangle is" + area +);
