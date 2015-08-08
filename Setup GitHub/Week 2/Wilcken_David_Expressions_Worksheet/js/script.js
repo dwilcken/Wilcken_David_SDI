111b4da8b1cd047d9f8b2f3c98e46d8029071ef5
david wilcken <dwilcken@gmail.com>
2015-08-08 10:56:39 -0400

    Slice of Pie Problem
    Slice of Pie Problem

--- Setup GitHub/Week 2/Wilcken_David_Expressions_Worksheet/js/script.js
+++ Setup GitHub/Week 2/Wilcken_David_Expressions_Worksheet/js/script.js
@@ -9,3 +9,15 @@ var sparkyAge = humanYears * dogYears;
 
 //multiply sparkysAge by dog years
 console.log("Sparky is "+humanYears+" years old in human years which is "+sparkyAge+" years in dog years");
+
+//Slice of pie part 1
+//A bunch of students are having a party and somebody ordered pizza. Create an expression that calculates
+//how much pizza each partygoer will get at the party.  (Assume all pizzas have the same number of slices
+//and that the person dividing the pizza is really precise, so this can be a decimal, like 3.52 slices, etc.)
+var slices = 8;
+var people = 35;
+var pizzas = 7;
+var slicesPerPerson = slices * pizzas / people;
+
+console.log("Each person ate "+slicesPerPerson+" slices of pizza at the party");
+
