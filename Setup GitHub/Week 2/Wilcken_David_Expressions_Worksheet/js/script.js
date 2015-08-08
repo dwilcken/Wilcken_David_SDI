79c0405602efc6038a3252a680570151710f332c
david wilcken <dwilcken@gmail.com>
2015-08-07 23:30:14 -0400

    First Formula Expressions Work Sheet
    First Formula Expressions Work Sheet

--- Setup GitHub/Week 2/Wilcken_David_Expressions_Worksheet/js/script.js
+++ Setup GitHub/Week 2/Wilcken_David_Expressions_Worksheet/js/script.js
@@ -1,3 +1,11 @@
-/**
- * Created by davidwilcken on 8/7/15.
- */
+//Expressions worksheet
+
+//Dog years problem
+//Dogs age 7 times faster than humans so a dog that is 1 year old in human years is 7 years old in “dog years.”
+//Calculate how old Sparky the pit bull is in dog years based on his actual age.
+var humanYears = 2;
+var dogYears = 7;
+var sparkyAge = humanYears * dogYears;
+
+//multiply sparkysAge by dog years
+console.log("Sparky is "+humanYears+" years old in human years which is "+sparkyAge+" years in dog years");
