d826880e5d3d299cd6e51753cc50e55797de2e93
david wilcken <dwilcken@gmail.com>
2015-08-08 14:38:38 -0400

    Expression Assignment Variables
    Expression Assignment Variables

--- Setup GitHub/Week 2/Wilcken_David_Expressions_Assignment/js/script.js
+++ Setup GitHub/Week 2/Wilcken_David_Expressions_Assignment/js/script.js
@@ -1,3 +1,13 @@
-/**
- * Created by davidwilcken on 8/8/15.
- */
+//David Wilcken
+//08 Aug 15
+//Expressions Assignment
+
+//Density Altitude Calculator
+//Density altitude = pressure altitude +(120 x (OAT - ISA temp))
+//Oat = Outside air temp
+//ISA = Standard temp which is found by - 2 degrees celsius per 1000ft of altitude
+//Pressure altitude is measured in inches of mercury.
+
+var pressureAltitude = "Station Altimeter Setting";
+var currentTemperature = "Outside Air Temperature";
+var stationElevation = "Station Elevation AGL";
