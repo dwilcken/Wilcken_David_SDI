deb5ec561f47c655e8e7b13384d5e34212bf9ef6
david wilcken <dwilcken@gmail.com>
2015-08-12 11:30:22 -0400

    Screencast Relational Expressions

--- Setup GitHub/Week 3/Wilcken_David_Screencast_Conditionals/js/script.js
+++ Setup GitHub/Week 3/Wilcken_David_Screencast_Conditionals/js/script.js
@@ -1,10 +1,11 @@
 //conditional logic
 
-var oldEnough = false;
+var kidHeight = 50;
+var minHeight = 48;
 
 //if the child is old enough, print to the console "you can ride!"
-if(oldEnough){
+//if the kid is over 48 inches in height
+if(kidHeight > minHeight){
     //code perfomed if condition is true
     console.log("You can ride the coaster!")
 }
-console.log("What comes after");
