796a333f12c9c4217dd216091e9d88d8b843c0e4
david wilcken <dwilcken@gmail.com>
2015-08-12 11:34:27 -0400

    Screencast Conditional with Expression

--- Setup GitHub/Week 3/Wilcken_David_Screencast_Conditionals/js/script.js
+++ Setup GitHub/Week 3/Wilcken_David_Screencast_Conditionals/js/script.js
@@ -1,11 +1,12 @@
 //conditional logic
 
-var kidHeight = 50;
+var kidHeight = 48;
 var minHeight = 48;
+var sneakerlift = 2;
 
 //if the child is old enough, print to the console "you can ride!"
 //if the kid is over 48 inches in height
-if(kidHeight > minHeight){
+if(kidHeight + sneakerlift > minHeight){
     //code perfomed if condition is true
     console.log("You can ride the coaster!")
 }
