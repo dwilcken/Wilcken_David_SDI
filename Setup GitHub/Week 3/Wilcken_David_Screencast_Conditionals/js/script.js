b81485a7f42143820fdf63ba3bd0b27a414c5c94
david wilcken <dwilcken@gmail.com>
2015-08-12 11:45:46 -0400

    Screencast Else If

--- Setup GitHub/Week 3/Wilcken_David_Screencast_Conditionals/js/script.js
+++ Setup GitHub/Week 3/Wilcken_David_Screencast_Conditionals/js/script.js
@@ -1,14 +1,19 @@
-//conditional logic
+//conditional logic - else if
 
-var kidHeight = 32;
+var kidHeight = 52;
 var minHeight = 48;
 var sneakerlift = 2;
+var wParentHeight = 45;
 
 //if the child is old enough, print to the console "you can ride!"
 //if the kid is over 48 inches in height
 if(kidHeight + sneakerlift > minHeight){
-    //code perfomed if condition is true
+    //you can ride!
     console.log("You can ride the coaster!")
+}else if(kidHeight > wParentHeight){
+    //you can ride with a parent present
+    console.log("You can ride, but only with a parent present")
 }else{
+    //sorry you have some growing to do
     console.log("Sorry kid, you have some growing to do.")
 }
