6d69e2881b9a011441c1ce91cd3f16f6c639d195
david wilcken <dwilcken@gmail.com>
2015-08-12 13:23:08 -0400

    Screencast Logical Operators

--- Setup GitHub/Week 3/Wilcken_David_Screencast_Conditionals/js/script.js
+++ Setup GitHub/Week 3/Wilcken_David_Screencast_Conditionals/js/script.js
@@ -17,3 +17,41 @@ if(kidHeight + sneakerlift > minHeight){
     //sorry you have some growing to do
     console.log("Sorry kid, you have some growing to do.")
 }
+
+//logical operators
+var budget = 100;
+var iPhonePrice = 199.99;
+var wonLottery = true;
+
+//if the price of the phone is less than our budget AND if our paycheck is over 300
+if(iPhonePrice < budget || wonLottery || wonLottery){
+    console.log("We can buy the phone");
+}else{
+    console.log("No phone for you!!");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
