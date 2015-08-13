641bb56b06efc52aea774c1a4b91d2f3ece5a997
david wilcken <dwilcken@gmail.com>
2015-08-13 14:21:07 -0400

    Last Chance For Gas

--- Setup GitHub/Week 3/Conditionals_Worksheet/js/script.js
+++ Setup GitHub/Week 3/Conditionals_Worksheet/js/script.js
@@ -3,8 +3,8 @@
  */
 
 //stuff your face
-//To get in the heavyweight divison of the Strawberry Festival's pie eating contest
-//you must weigh 250lbs or more. Determine wheter an entrant qualifies based on his weight.
+//To get in the heavyweight division of the Strawberry Festival's pie eating contest
+//you must weigh 250lbs or more. Determine whether an entrant qualifies based on his weight.
 
 var weight = 300;
 if(weight <= 250){
@@ -13,3 +13,19 @@ if(weight <= 250){
     console.log("The competitor qualifies for the heavyweight division");
 }
 
+//Last chance for gas
+//A driver has to determine if they can make it across the desert with their current fuel.
+//They are about to pass the last gas station for the next 200 miles, and they need to
+//determine whether they should stop now for gas or not.
+
+var milePerGallon = 18;
+var amountInTank = .10;
+var amountOfTank = 15;
+var miles = 200;
+//.75% of 15 gal tank is = 11.25
+if(amountInTank * amountOfTank > miles / milePerGallon){
+    console.log("Yes, you can make it without stopping for gas!")
+}else{
+    console.log("You only have "+amountInTank * amountOfTank+" gallons in your tank," +
+        "you had better get gas now while you can.")
+}
