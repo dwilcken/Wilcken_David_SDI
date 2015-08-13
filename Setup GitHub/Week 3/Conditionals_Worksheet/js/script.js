ba8385720fc1bc470ed4be200df8affd8d589991
david wilcken <dwilcken@gmail.com>
2015-08-13 13:16:05 -0400

    Stuff Your Face

--- Setup GitHub/Week 3/Conditionals_Worksheet/js/script.js
+++ Setup GitHub/Week 3/Conditionals_Worksheet/js/script.js
@@ -1,3 +1,15 @@
 /**
  * Created by davidwilcken on 8/13/15.
  */
+
+//stuff your face
+//To get in the heavyweight divison of the Strawberry Festival's pie eating contest
+//you must weigh 250lbs or more. Determine wheter an entrant qualifies based on his weight.
+
+var weight = 300;
+if(weight <= 250){
+    console.log("The competitor needs to gain some weight!");
+}else{
+    console.log("The competitor qualifies for the heavyweight division");
+}
+
