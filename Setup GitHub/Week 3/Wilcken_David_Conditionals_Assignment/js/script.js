3003daceae2d7e7da65132e366ba09ba0c53b2a4
david wilcken <dwilcken@gmail.com>
2015-08-14 11:44:45 -0400

    Conditionals Assignment Test 2

--- Setup GitHub/Week 3/Wilcken_David_Conditionals_Assignment/js/script.js
+++ Setup GitHub/Week 3/Wilcken_David_Conditionals_Assignment/js/script.js
@@ -11,24 +11,21 @@ var august = "Archery";
 var september = "Crossbow";
 var october = "Muzzleloading";
 var november = "General Gun";
-
 var minAge = 17;
 
-if(howOld > minAge){
-    console.log("Congratulations you are old enough to hunt without supervision");
-}else if(huntingMonth === august){
-    console.log("You can hunt "+august+" this season");
-}else if(huntingMonth === september){
-    console.log("You can hunt "+september+" this season");
-}else if(huntingMonth === october){
-    console.log("You can hunt "+october+" this season");
-}else if(huntingMonth === november){
-    console.log("You can hunt "+november+" this season");
-}else{
-    console.log("You will need an adult to take you hunting");
-}
 
 if(howOld > minAge && huntingMonth === august){
     console.log("Congratulations you are old enough to hunt "+august+" this season " +
         "without an adult")
+}else if(huntingMonth === september) {
+    console.log("Congratulations you are old enough to hunt "+september+" this season " +
+        "without an adult");
+}else if(huntingMonth === october){
+    console.log("Congratulations you are old enough to hunt "+october+" this season" +
+        "without an adult");
+}else if (huntingMonth === november){
+    console.log("Congratulations you are old enough to hunt "+november+" this season" +
+        "without an adult");
+}else{
+    console.log("Sorry you will need an adult to go hunt with you for all seasons this year");
 }
