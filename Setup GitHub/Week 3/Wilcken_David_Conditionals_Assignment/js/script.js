baa39959e1a0ee7ac53cb09501e56b67c9e86dea
david wilcken <dwilcken@gmail.com>
2015-08-14 12:40:05 -0400

    Conditionals Test 3

--- Setup GitHub/Week 3/Wilcken_David_Conditionals_Assignment/js/script.js
+++ Setup GitHub/Week 3/Wilcken_David_Conditionals_Assignment/js/script.js
@@ -6,26 +6,17 @@
 
 var howOld = prompt("Enter your age");
 var huntingMonth = prompt("Enter which month you would like to hunt.");
-//var noHunt = ['December','January','February','March','April','May','June','July'];
-var august = "Archery";
-var september = "Crossbow";
-var october = "Muzzleloading";
-var november = "General Gun";
+var noHunt = ["December","January","February","March","April","May","June","July"];
+var canHunt = ["August", "September", "October", "November"];
 var minAge = 17;
 
 
-if(howOld > minAge && huntingMonth === august){
-    console.log("Congratulations you are old enough to hunt "+august+" this season " +
-        "without an adult")
-}else if(huntingMonth === september) {
-    console.log("Congratulations you are old enough to hunt "+september+" this season " +
-        "without an adult");
-}else if(huntingMonth === october){
-    console.log("Congratulations you are old enough to hunt "+october+" this season" +
-        "without an adult");
-}else if (huntingMonth === november){
-    console.log("Congratulations you are old enough to hunt "+november+" this season" +
-        "without an adult");
+if(howOld > minAge && huntingMonth == canHunt){
+    console.log("You are old enough and may hunt this month");
+}else if(howOld > minAge && huntingMonth == noHunt){
+    console.log("Sorry.You are old enough but you cannot during the month selected");
+}else if(howOld < minAge && huntingMonth == canHunt){
+    console.log("You will need an adult to hunt with you for the month selected");
 }else{
-    console.log("Sorry you will need an adult to go hunt with you for all seasons this year");
+    console.log("You will not be able to hunt this month")
 }
