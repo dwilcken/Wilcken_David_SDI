9fc3f6eeca3a635ee3155a975c0091d7505c0e9c
david wilcken <dwilcken@gmail.com>
2015-08-14 11:35:50 -0400

    Conditional Assignment Trial 1

--- Setup GitHub/Week 3/Wilcken_David_Conditionals_Assignment/js/script.js
+++ Setup GitHub/Week 3/Wilcken_David_Conditionals_Assignment/js/script.js
@@ -4,7 +4,31 @@
 
 //Hunting calculator for season and age
 
-var = prompt("Current Date");
-var age = 15;
+var howOld = prompt("Enter your age");
+var huntingMonth = prompt("Enter which month you would like to hunt.");
+//var noHunt = ['December','January','February','March','April','May','June','July'];
+var august = "Archery";
+var september = "Crossbow";
+var october = "Muzzleloading";
+var november = "General Gun";
 
+var minAge = 17;
 
+if(howOld > minAge){
+    console.log("Congratulations you are old enough to hunt without supervision");
+}else if(huntingMonth === august){
+    console.log("You can hunt "+august+" this season");
+}else if(huntingMonth === september){
+    console.log("You can hunt "+september+" this season");
+}else if(huntingMonth === october){
+    console.log("You can hunt "+october+" this season");
+}else if(huntingMonth === november){
+    console.log("You can hunt "+november+" this season");
+}else{
+    console.log("You will need an adult to take you hunting");
+}
+
+if(howOld > minAge && huntingMonth === august){
+    console.log("Congratulations you are old enough to hunt "+august+" this season " +
+        "without an adult")
+}
