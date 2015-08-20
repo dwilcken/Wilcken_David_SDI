fb044fdfd5b3d09e0ec90511f84fbb95d530e8cb
david wilcken <dwilcken@gmail.com>
2015-08-20 12:50:53 -0400

    Circumference

--- Setup GitHub/Week 4/Activity Functions Worksheet/js/script.js
+++ Setup GitHub/Week 4/Activity Functions Worksheet/js/script.js
@@ -1,2 +1,25 @@
-//Calculate the Area of a Rectangle
+/*Calculate the Area of a Rectangle
 
+
+var width = 5;
+var height = 6;
+var area = calculateArea(width, height);
+console.log("The area of the rectangle is "+area+".");
+
+function calculateArea(w, h )
+{
+    return w *h;
+}
+*/
+
+
+//Circumference - Calculate the circumference of a circle
+
+
+function area(){
+    var radius = 10;
+    var pi = 3.14;
+    var area = radius * (pi * pi);
+    console.log("The area of your circle is "+area+".");
+}
+area()
