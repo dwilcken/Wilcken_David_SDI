db8fa9fa1c3ebcabca44dbc513d35e60b159df03
david wilcken <dwilcken@gmail.com>
2015-08-20 11:03:11 -0400

    Function Invocation

---
+++ Setup GitHub/Week 4/Screencast Functions/js/script.js
@@ -0,0 +1,14 @@
+//Functions - Basic Structure
+
+function outptMsg(){
+    console.log("Hello World!");
+}
+
+function calcArea (){
+    var width = 20;
+    var height = 30;
+    var area = width * height
+    console.log(area);
+}
+
+calcArea();
