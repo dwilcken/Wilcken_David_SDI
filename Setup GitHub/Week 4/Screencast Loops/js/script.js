db581cff6b43a60ebc1d32a188ebb0a2fca402ce
david wilcken <dwilcken@gmail.com>
2015-08-21 09:52:04 -0400

    While Loops

---
+++ Setup GitHub/Week 4/Screencast Loops/js/script.js
@@ -0,0 +1,8 @@
+//While Loops
+
+var b = 10;//sets up the index
+
+while ( b > 0){ //checks the condition
+    console.log(b + ' kegs on the wall');
+    b--; //increments or decrements the index
+}
