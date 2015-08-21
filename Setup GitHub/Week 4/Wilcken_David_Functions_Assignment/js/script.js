869856e9e32aa0ab8086acfde0d9467a28d9798f
david wilcken <dwilcken@gmail.com>
2015-08-21 14:24:57 -0400

    Loop Test 3

--- Setup GitHub/Week 4/Wilcken_David_Functions_Assignment/js/script.js
+++ Setup GitHub/Week 4/Wilcken_David_Functions_Assignment/js/script.js
@@ -1,37 +1,37 @@
-for (var i = 6; i > 0; i--) {
+//loop for FL Lotto
+for (var f = 6; f > 0; f--) {
     console.log("Florida Lottery " + floridaRandom());
 }
 
-for (var i = 5; i > 0; i--) {
+//loop forPB lotto
+for (var p = 5; p > 0; p--) {
     console.log("PB Lottery " + lotteryRandom());
 }
 
-for (var i = 1; i > 0; i--) {
+//loop for the PB
+for (var b = 1; b > 0; b--) {
     console.log("Your Powerball is: " + powerballRandom());
 }
 
 //Florida lottery 6 random number 1-53
 function floridaRandom(min,max){
-    var min = 1;
-    var max = 53;
-    return Math.floor(Math.random() * (max - min +1)) +min;
+    var fMin = 1;
+    var fMax = 53;
+    return Math.floor(Math.random() * (fMax - fMin +1)) +fMin;
 }
 
 //console.log(floridaRandom());
 
 //Powerball lottery 5 random number 1-59 and powerball 1-35
 function lotteryRandom(min, max){
-    var min = 1;
-    var max = 59;
-    return Math.floor(Math.random() * (max - min +1)) + min;
+    var pMin = 1;
+    var pMax = 59;
+    return Math.floor(Math.random() * (pMax - pMin +1)) + pMin;
 }
-
 
 //The powerball random 1-35
 function powerballRandom(min, max){
-    var min = 1;
-    var max = 35;
-    return Math.floor(Math.random() * (max - min +1))+min;
+    var bMin = 1;
+    var bMax = 35;
+    return Math.floor(Math.random() * (bMax - bMin +1))+bMin;
 }
-
-
