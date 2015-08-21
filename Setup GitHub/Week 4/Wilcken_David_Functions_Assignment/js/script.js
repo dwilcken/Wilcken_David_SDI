71dd71415dbb058aa423c6a8d772f5bb4d99701b
david wilcken <dwilcken@gmail.com>
2015-08-21 18:36:43 -0400

    Think Im Going The Right Direction

--- Setup GitHub/Week 4/Wilcken_David_Functions_Assignment/js/script.js
+++ Setup GitHub/Week 4/Wilcken_David_Functions_Assignment/js/script.js
@@ -1,37 +1,27 @@
-//loop for FL Lotto
-for (var f = 6; f > 0; f--) {
-    console.log("Florida Lottery " + floridaRandom());
+function lottoCreator(min, max) {
+
+    console.log(min + " " + max);
+
+
+    var myArray =[];
+
+    for (var i = 0; i < 2; i++){
+
+        myArray[0] = Math.floor(Math.random() * (max - min +1)) +min;
+        myArray[1] = Math.floor(Math.random() * (max - min +1)) +min;
+        myArray[2] = Math.floor(Math.random() * (max - min +1)) +min;
+        myArray[3] = Math.floor(Math.random() * (max - min +1)) +min;
+        myArray[4] = Math.floor(Math.random() * (max - min +1)) +min;
+        myArray[5] = Math.floor(Math.random() * (max - min +1)) +min;
+    }
+
+    return myArray
 }
 
-//loop forPB lotto
-for (var p = 5; p > 0; p--) {
-    console.log("PB Lottery " + lotteryRandom());
-}
 
-//loop for the PB
-for (var b = 1; b > 0; b--) {
-    console.log("Your Powerball is: " + powerballRandom());
-}
 
-//Florida lottery 6 random number 1-53
-function floridaRandom(min,max){
-    var fMin = 1;
-    var fMax = 53;
-    return Math.floor(Math.random() * (fMax - fMin +1)) +fMin;
-}
+var flLotto = lottoCreator(1, 53);
+ console.log(flLotto);
 
-//console.log(floridaRandom());
-
-//Powerball lottery 5 random number 1-59 and powerball 1-35
-function lotteryRandom(min, max){
-    var pMin = 1;
-    var pMax = 59;
-    return Math.floor(Math.random() * (pMax - pMin +1)) + pMin;
-}
-
-//The powerball random 1-35
-function powerballRandom(min, max){
-    var bMin = 1;
-    var bMax = 35;
-    return Math.floor(Math.random() * (bMax - bMin +1))+bMin;
-}
+var pbLotto = lottoCreator(1,59);
+console.log(pbLotto);
