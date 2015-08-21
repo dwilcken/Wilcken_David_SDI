2c19774e401c2cafb041b6734942f39bf3f12c1e
david wilcken <dwilcken@gmail.com>
2015-08-21 19:41:26 -0400

    Loop Fix 4

--- Setup GitHub/Week 4/Wilcken_David_Functions_Assignment/js/script.js
+++ Setup GitHub/Week 4/Wilcken_David_Functions_Assignment/js/script.js
@@ -1,27 +1,17 @@
 function lottoCreator(min, max) {
 
-    console.log(min + " " + max);
-
-
     var myArray =[];
 
-    for (var i = 0; i < 2; i++){
+    for (var i = 0; i < 6; i++){
 
-        myArray[0] = Math.floor(Math.random() * (max - min +1)) +min;
-        myArray[1] = Math.floor(Math.random() * (max - min +1)) +min;
-        myArray[2] = Math.floor(Math.random() * (max - min +1)) +min;
-        myArray[3] = Math.floor(Math.random() * (max - min +1)) +min;
-        myArray[4] = Math.floor(Math.random() * (max - min +1)) +min;
-        myArray[5] = Math.floor(Math.random() * (max - min +1)) +min;
+        myArray[i] = Math.floor(Math.random() * (max - min +1)) +min;
     }
 
     return myArray
 }
 
-
-
 var flLotto = lottoCreator(1, 53);
- console.log(flLotto);
+console.log("Your Florida State Lottery Numbers: "+flLotto+".");
 
 var pbLotto = lottoCreator(1,59);
-console.log(pbLotto);
+console.log("Your Powerball Numbers: "+pbLotto+".");
