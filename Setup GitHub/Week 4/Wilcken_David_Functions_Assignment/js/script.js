94661a9d4423ec827a2348702eeef9382a414138
david wilcken <dwilcken@gmail.com>
2015-08-22 21:59:34 -0400

    Final Function

--- Setup GitHub/Week 4/Wilcken_David_Functions_Assignment/js/script.js
+++ Setup GitHub/Week 4/Wilcken_David_Functions_Assignment/js/script.js
@@ -1,5 +1,6 @@
-var flLotto = lottoCreator(1, 53);
+var flLotto = lottoCreator(1,53);
 var pbLotto = pblottoCreator(1,59);
+var bigPb = bigPbCreator(1,35);
 
 
 var whichLotto = prompt("Please enter which lotto you are playing.\nFlorida State or Powerball.");
@@ -9,7 +10,7 @@ var whichLotto = prompt("Please enter which lotto you are playing.\nFlorida Stat
     }else if(whichLotto == "Florida State"){
         console.log("Your Florida State Lottery Numbers Are: "+flLotto+".");
     }else if(whichLotto == "Powerball"){
-        console.log("Your Powerball Numbers Are: "+pbLotto+".");
+        console.log("Your Powerball Numbers Are: "+pbLotto+" Powerball: "+bigPb+".");
     }
 
 
@@ -28,7 +29,7 @@ function lottoCreator(min, max) {
 function pblottoCreator (min, max){
 
     var myArray =[];
-    
+
     for (var i = 0; i < 5; i++){
         myArray[i] = Math.floor(Math.random() * (max - min +1)) +min;
     }
@@ -36,8 +37,12 @@ function pblottoCreator (min, max){
     return myArray
 }
 
+function bigPbCreator (min, max){
 
-//console.log("Your Florida State Lottery Numbers: "+flLotto+".");
+    var myArray = [];
 
-
-//console.log("Your Powerball Numbers: "+pbLotto+".");
+    for (var i =0; i < 1; i++){
+        myArray [i] = Math.floor(Math.random() *(max - min +1)) +min;
+    }
+    return myArray
+}
