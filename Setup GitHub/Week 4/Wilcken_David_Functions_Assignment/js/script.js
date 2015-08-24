3a24120196405ee2a83f0710da124aefe47cd9d0
david wilcken <dwilcken@gmail.com>
2015-08-23 20:49:18 -0400

    Final Comments

--- Setup GitHub/Week 4/Wilcken_David_Functions_Assignment/js/script.js
+++ Setup GitHub/Week 4/Wilcken_David_Functions_Assignment/js/script.js
@@ -4,10 +4,9 @@ var bigPb = bigPbCreator(1,35);//assigning var to bigPbCreator function and sett
 
 
 var whichLotto = prompt("Please enter which lotto you are playing.\nFlorida State or Powerball.");
-    if (whichLotto == "") {//prompt validation
+    while (whichLotto == "") {//prompt validation set on a loop if no entry is made
         whichLotto = prompt("Please enter which lotto.\nFlorida State or Powerball.");//reminder to the user to enter a value
-        console.log("Thank you.");//log thanking the user for their input
-    }
+}
 if(whichLotto == "Florida State"){//condition if Florida State is selected
         console.log("Your Florida State Lottery Numbers Are: "+flLotto+".");//out put for FS
     }else if(whichLotto == "Powerball"){//condition if Powerball is selected
@@ -15,35 +14,35 @@ if(whichLotto == "Florida State"){//condition if Florida State is selected
     }
 
 
-function lottoCreator(min, max) {//
+function lottoCreator(min, max) {//creating function with function parameter
 
-    var myArray =[];
+    var myArray =[];//creating an array var for the function
 
-    for (var i = 0; i < 6; i++){
+    for (var i = 0; i < 6; i++){//creating a loop to repeat the code below 6 times
 
-        myArray[i] = Math.floor(Math.random() * (max - min +1)) +min;
+        myArray[i] = Math.floor(Math.random() * (max - min +1)) +min;//code that selects a random #
     }
 
-    return myArray
+    return myArray// function return that stops the function
 }
 
-function pblottoCreator (min, max){
+function pblottoCreator (min, max){//creating function with function parameter
 
-    var myArray =[];
+    var myArray =[];//creating an array var for the function
 
-    for (var i = 0; i < 5; i++){
-        myArray[i] = Math.floor(Math.random() * (max - min +1)) +min;
+    for (var i = 0; i < 5; i++){//creating a loop to repeat the code below 5 times
+        myArray[i] = Math.floor(Math.random() * (max - min +1)) +min;//code that selects a random #
     }
 
-    return myArray
+    return myArray// function return that stops the function
 }
 
-function bigPbCreator (min, max){
+function bigPbCreator (min, max){//creating function with function parameter
 
-    var myArray = [];
+    var myArray = [];//creating an array var for the function
 
-    for (var i =0; i < 1; i++){
-        myArray [i] = Math.floor(Math.random() *(max - min +1)) +min;
+    for (var i =0; i < 1; i++){//creating a loop to repeat the code below 1 time
+        myArray [i] = Math.floor(Math.random() *(max - min +1)) +min;//code that selects a random #
     }
-    return myArray
+    return myArray//function return that stops the function
 }
