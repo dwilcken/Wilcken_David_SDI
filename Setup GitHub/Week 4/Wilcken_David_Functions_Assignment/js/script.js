9b6834208007358c5da741ff52c5a5cdcbb893c0
david wilcken <dwilcken@gmail.com>
2015-08-25 10:29:39 -0400

    Organize Code

--- Setup GitHub/Week 4/Wilcken_David_Functions_Assignment/js/script.js
+++ Setup GitHub/Week 4/Wilcken_David_Functions_Assignment/js/script.js
@@ -1,10 +1,44 @@
+/*
+  David Wilcken
+  SDI
+  Functions Assignment
+ */
+
+//variables
 var flLotto = lottoCreator(1,53);//assigning var to lottoCreator function and setting min & max
 var pbLotto = pblottoCreator(1,59);//assigning var to lottoCreator function and setting min & max
 var bigPb = bigPbCreator(1,35);//assigning var to bigPbCreator function and setting min & max
 
 
+//functions
+function lottoCreator(min, max) {//creating function with function parameter
+    var myArray =[];//creating an array var for the function
+    for (var i = 0; i < 6; i++){//creating a loop to repeat the code below 6 times
+        myArray[i] = Math.floor(Math.random() * (max - min +1)) +min;//code that selects a random #
+    }
+    return myArray// function return that stops the function
+}
+
+function pblottoCreator (min, max){//creating function with function parameter
+    var myArray =[];//creating an array var for the function
+    for (var i = 0; i < 5; i++){//creating a loop to repeat the code below 5 times
+        myArray[i] = Math.floor(Math.random() * (max - min +1)) +min;//code that selects a random #
+    }
+    return myArray// function return that stops the function
+}
+
+function bigPbCreator (min, max){//creating function with function parameter
+    var myArray = [];//creating an array var for the function
+    for (var i =0; i < 1; i++){//creating a loop to repeat the code below 1 time
+        myArray [i] = Math.floor(Math.random() *(max - min +1)) +min;//code that selects a random #
+    }
+    return myArray//function return that stops the function
+}
+
+
+//main code
 var whichLotto = prompt("Please enter which lotto you are playing.\nFlorida State or Powerball.");
-    while (whichLotto == "") {//prompt validation set on a loop if no entry is made
+    while (whichLotto === "") {//prompt validation set on a loop if no entry is made
         whichLotto = prompt("Please enter which lotto.\nFlorida State or Powerball.");//reminder to the user to enter a value
 }
 if(whichLotto == "Florida State"){//condition if Florida State is selected
@@ -13,36 +47,3 @@ if(whichLotto == "Florida State"){//condition if Florida State is selected
         console.log("Your Powerball Numbers Are: "+pbLotto+" Powerball: "+bigPb+".");//out put for PB
     }
 
-
-function lottoCreator(min, max) {//creating function with function parameter
-
-    var myArray =[];//creating an array var for the function
-
-    for (var i = 0; i < 6; i++){//creating a loop to repeat the code below 6 times
-
-        myArray[i] = Math.floor(Math.random() * (max - min +1)) +min;//code that selects a random #
-    }
-
-    return myArray// function return that stops the function
-}
-
-function pblottoCreator (min, max){//creating function with function parameter
-
-    var myArray =[];//creating an array var for the function
-
-    for (var i = 0; i < 5; i++){//creating a loop to repeat the code below 5 times
-        myArray[i] = Math.floor(Math.random() * (max - min +1)) +min;//code that selects a random #
-    }
-
-    return myArray// function return that stops the function
-}
-
-function bigPbCreator (min, max){//creating function with function parameter
-
-    var myArray = [];//creating an array var for the function
-
-    for (var i =0; i < 1; i++){//creating a loop to repeat the code below 1 time
-        myArray [i] = Math.floor(Math.random() *(max - min +1)) +min;//code that selects a random #
-    }
-    return myArray//function return that stops the function
-}
